e2490ca479ef4f9e63358639ae032e09f04ddb35
tamakuma-kengo <b1801952@planet.kanazawa-it.ac.jp>
2021-11-25 14:16:36 +0900

    dataup csv

--- course/filemodules/csvwriter.py
+++ course/filemodules/csvwriter.py
@@ -7,9 +7,9 @@ def adder(user,course,score,start,end):
     list1.append(list2)
 """
 
-def csvwriter(user,grade,start_time,end_time):
-    with open("./course/csvfiles/students.csv","w") as f:
+def csvwriter(user,course,grade,start_time,end_time):
+    with open("./course/csvfiles/students.csv","a") as f:
         writer = csv.writer(f)
-        list2 = [user,grade,start_time,end_time]
+        list2 = [user,course,grade,start_time,end_time]
         list1.append(list2)
         writer.writerows(list1)
